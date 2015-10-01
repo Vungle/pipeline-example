a143183810c3e137a419abb125e3091af8097d15
Eddie Simeon <eddie.simeon@vungle.com>
2015-09-30 23:09:23 -0700

    fix tests
    fix tests

--- test/converter.js
+++ test/converter.js
@@ -20,8 +20,10 @@ describe("Color Code Converter", function() {
       var green = converter.hexToRgb("00ff00");
       var blue  = converter.hexToRgb("0000ff");
 
-//      expect(red).to.deep.equal([255, 0, 0]);
-      expect(red).to.deep.equal([255, 255, 0]);
+// good test
+      expect(red).to.deep.equal([255, 0, 0]);
+// bad test
+//      expect(red).to.deep.equal([255, 255, 0]);
       expect(green).to.deep.equal([0, 255, 0]);
       expect(blue).to.deep.equal([0, 0, 255]);
     });
